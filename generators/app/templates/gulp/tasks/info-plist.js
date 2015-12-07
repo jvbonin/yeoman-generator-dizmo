4d1bf410cda8649ed9cad43fa9c8108573ae58b8
Hasan Karahan <hasan.karahan@blackhan.com>
2015-12-07 14:59:47 +0400

    DD-1094: migrated to gulp-plist

--- generators/app/templates/gulp/tasks/info-plist.js
+++ generators/app/templates/gulp/tasks/info-plist.js
@@ -1,28 +1,19 @@
 var pkg = require('../package.js');
 var gulp = require('gulp'),
-    gulp_mustache = require('gulp-mustache'),
+    gulp_plist = require('gulp-plist'),
     gulp_rename = require('gulp-rename');
 
 gulp.task('info.plist', function () {
     return gulp.src('.info.plist')
-        .pipe(gulp_mustache({
-            ALLOW_RESIZE: false,
-            API_VERSION: '1.3',
-            BUNDLE_DISPLAY_NAME: pkg.name,
-            BUNDLE_IDENTIFIER: 'com.example.my_dizmo',
-            BUNDLE_NAME: pkg.name,
-            BUNDLE_VERSION: pkg.version,
-            CATEGORY: 'none',
-            DESCRIPTION: pkg.description,
-            ELEMENTS_VERSION: '1.0',
-            FORCE_UPDATE: false,
-            HEIGHT: 240,
-            HIDDEN_DIZMO: false,
-            MAIN_HTML: 'index.html',
-            MIN_SPACE_VERSION: '0.0.0',
-            TAGS: pkg.keywords,
-            TITLE_EDITABLE: true,
-            WIDTH: 480
+        .pipe(gulp_plist({
+            BundleDisplayName: pkg.name,
+            BundleIdentifier: '<%= bundleId %>',
+            BundleName: pkg.name,
+            BundleVersion: pkg.version,
+            Description: pkg.description,
+            Height: 240,
+            Tags: pkg.keywords,
+            Width: 480
         }))
         .pipe(gulp_rename('Info.plist'))
         .pipe(gulp.dest('build/{0}/'.replace('{0}', pkg.name)));
