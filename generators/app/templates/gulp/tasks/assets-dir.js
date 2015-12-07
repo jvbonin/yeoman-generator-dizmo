03dd089d2a4945aad3a069c72fbc0fea10bf526e
Hasan Karahan <hasan.karahan@blackhan.com>
2015-12-07 14:25:50 +0400

    DD-1094: Gulp task fixes

--- generators/app/templates/gulp/tasks/assets-dir.js
+++ generators/app/templates/gulp/tasks/assets-dir.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
     gulp_copy = require('gulp-copy');
 
 gulp.task('assets.dir:copy', function () {
-    return gulp.src('assets/**/*')
-        .pipe(gulp_copy('build/{0}/'.replace('{0}', pkg.name), {
-            prefix: 1
-        }));
+    return gulp.src([
+        'assets/Icon.*', 'assets/Icon-dark.*', 'assets/Preview.*'
+    ])
+    .pipe(gulp_copy('build/{0}/'.replace('{0}', pkg.name), {
+        prefix: 1
+    }));
 });
 gulp.task('assets.dir', ['assets.dir:copy'], function () {
     return gulp.src('assets/**/*')
--- generators/app/templates/gulp/tasks/style-css.js
+++ generators/app/templates/gulp/tasks/style-css.js
@@ -4,13 +4,13 @@ var gulp = require('gulp'),
     gulp_sass = require('gulp-sass'),
     gulp_sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('style.css:image', function () {
-    return gulp.src('src/style/image/**/*')
-        .pipe(gulp_copy('build/{0}/style/image/'.replace('{0}', pkg.name), {
-            prefix: 3
+gulp.task('style.css:copy', function () {
+    return gulp.src(['src/style/**/*', '!src/style/**/*.scss'])
+        .pipe(gulp_copy('build/{0}/style/'.replace('{0}', pkg.name), {
+            prefix: 2
         }));
 });
-gulp.task('style.css', ['style.css:image'], function () {
+gulp.task('style.css', ['style.css:copy'], function () {
     return gulp.src('src/style/*.scss')
         .pipe(gulp_sourcemaps.init())
         .pipe(gulp_sass({outputStyle: 'compressed'})
