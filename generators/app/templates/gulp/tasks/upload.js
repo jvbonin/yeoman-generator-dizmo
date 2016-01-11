29a5f6f9e951c8408f1883aaa65cabc825d94644
Hasan Karahan <hasan.karahan@blackhan.com>
2016-01-11 15:14:24 +0400

    DIG-12: assert upload URL

--- generators/app/templates/gulp/tasks/upload.js
+++ generators/app/templates/gulp/tasks/upload.js
@@ -16,39 +16,48 @@ gulp.task('upload', ['build:zip'], function () {
     var upload_pwd = process.env.DIZMO_CREDENTIALS_PASSWORD
         || pkg.dizmo.credentials && pkg.dizmo.credentials.password;
 
-    if (upload_url) {
-        var options = url.parse(upload_url),
-            path = 'build/{0}-{1}.dzm'
-                .replace('{0}', pkg.name)
-                .replace('{1}', pkg.version);
+    assert(upload_url, 'package.dizmo.urls.upload required');
 
-        var auth = '';
-        if (upload_usr) {
-            if (upload_pwd) {
-                auth += upload_usr + ':' + upload_pwd;
-            } else {
-                auth += upload_usr;
-            }
+    var options = url.parse(upload_url),
+        path = 'build/{0}-{1}.dzm'
+            .replace('{0}', pkg.name)
+            .replace('{1}', pkg.version);
+
+    var auth = '';
+    if (upload_usr) {
+        if (upload_pwd) {
+            auth += upload_usr + ':' + upload_pwd;
         } else {
-            if (upload_pwd) {
-                auth += upload_pwd;
-            }
+            auth += upload_usr;
         }
-        if (auth && !options.auth) {
-            lodash.extend(options, {auth: auth});
+    } else {
+        if (upload_pwd) {
+            auth += upload_pwd;
         }
-
-        fs.readFile(path, function (err, data) {
-            if (!err) {
-                var req = http.request(lodash.extend(options, {
-                    method: 'POST', headers: {'Content-Length': data.length}
-                }));
-
-                req.write(data);
-                req.end();
-            }
-
-            else throw err;
-        });
     }
+    if (auth && !options.auth) {
+        lodash.extend(options, {auth: auth});
+    }
+
+    fs.readFile(path, function (err, data) {
+        if (!err) {
+            var opt = lodash.extend(options, {
+                method: 'POST', headers: {'Content-Length': data.length}
+            });
+
+            var req = http.request(opt, function (res) {
+             // console.log('[status]', res.statusCode);
+             // console.log('[header]', JSON.stringify(res.headers));
+            });
+
+            req.on('error', function (error) {
+                console.log('[REQ:ERROR]', error);
+            });
+
+            req.write(data);
+            req.end();
+        }
+
+        else throw err;
+    });
 });
